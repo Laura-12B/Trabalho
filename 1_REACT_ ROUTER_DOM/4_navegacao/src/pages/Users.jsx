860635bfbf856d2c398ccc539d8eff38315b982b
laura <laura.ochoa.medeiros.2018@gmail.com>
2023-08-24 16:53:32 -0300

    atividade

---
+++ 1_REACT_ ROUTER_DOM/4_navegacao/src/pages/Users.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Users() {
+  return (
+    <div>
+      <h1>Usuarios</h1>
+      <Link to={"/"}>Voltar</Link>
+    </div>
+  )
+}
+
+export default Users

7e7eceb4e8a80a6c39d6952d712e65c7c9f28148
laura <laura.ochoa.medeiros.2018@gmail.com>
2023-09-14 16:58:28 -0300

    comentario

--- 1_REACT_ ROUTER_DOM/4_navegacao/src/pages/Home.jsx
+++ 1_REACT_ ROUTER_DOM/4_navegacao/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Lovi u</h1>
       <Link to={"/usuarios"}>Ver Usuários</Link>
     </div>
   )
